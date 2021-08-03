dc235e835c4c8e4fdb2abc5711ecc39147125fe3
Abhinandan Kushwaha <kushabhi5@gmail.com>
2021-08-03 19:19:27 +0530

    added props.colors|

--- Fireworks.js
+++ Fireworks.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Dimensions, StyleSheet, View, Animated, Easing } from 'react-native';
+import React, {Component} from 'react';
+import {Dimensions, StyleSheet, View, Animated, Easing} from 'react-native';
 
 let skyHeight;
 let skyWidth;
@@ -21,7 +21,7 @@ export default class Fireworks extends Component {
   }
 
   componentWillMount = () => {
-    const { height, width, zIndex, iterations, circular } = this.props;
+    const {height, width, zIndex, iterations, circular} = this.props;
     if (circular) {
       this.fillXYpositions();
     }
@@ -56,15 +56,15 @@ export default class Fireworks extends Component {
       xPositions[i + 5] = x;
       xPositions[i + 10] = 200 - x;
       xPositions[i + 15] = 200 - x;
-      yPositions[i] = 100 - (Math.sqrt(10000 - (x - 100) * (x - 100)));
-      yPositions[i + 5] = 100 + (Math.sqrt(10000 - (x - 100) * (x - 100)));
-      yPositions[i + 10] = 100 - (Math.sqrt(10000 - (x - 100) * (x - 100)));
-      yPositions[i + 15] = 100 + (Math.sqrt(10000 - (x - 100) * (x - 100)));
+      yPositions[i] = 100 - Math.sqrt(10000 - (x - 100) * (x - 100));
+      yPositions[i + 5] = 100 + Math.sqrt(10000 - (x - 100) * (x - 100));
+      yPositions[i + 10] = 100 - Math.sqrt(10000 - (x - 100) * (x - 100));
+      yPositions[i + 15] = 100 + Math.sqrt(10000 - (x - 100) * (x - 100));
     }
-  }
+  };
 
   setExplosionSpots = (shouldUpdateCounts) => {
-    let { density } = this.props;
+    let {density} = this.props;
     if (density && typeof density === 'number' && density > 0) {
       if (density > 10) {
         density = 10;
@@ -79,7 +79,7 @@ export default class Fireworks extends Component {
       x[i] = this.getRandom(skyWidth);
       y[i] = this.getRandom(skyHeight);
     }
-    this.setState({ x, y }, () => {
+    this.setState({x, y}, () => {
       if (explosionIterations && typeof explosionIterations === 'number') {
         if (shouldUpdateCounts) {
           count++;
@@ -96,20 +96,18 @@ export default class Fireworks extends Component {
   };
 
   animateOpacity() {
-    const { speed } = this.props;
+    const {speed} = this.props;
     this.fadingOpacity.setValue(1);
     Animated.timing(this.fadingOpacity, {
       toValue: 0,
       duration: speed === 1 ? 900 : speed === 3 ? 500 : 700,
       easing: Easing.ease,
       useNativeDriver: false,
-    }).start(() =>
-      this.setExplosionSpots(true)
-    );
+    }).start(() => this.setExplosionSpots(true));
   }
 
   animateBall() {
-    const { speed } = this.props;
+    const {speed} = this.props;
     this.movingBall.setValue(0);
     Animated.timing(this.movingBall, {
       toValue: 1,
@@ -124,36 +122,40 @@ export default class Fireworks extends Component {
   };
 
   getRandomColors = () => {
-    const { colors } = this.props;
+    const {colors} = this.props;
     if (colors && colors.length > 0) {
       let l = colors.length - 1;
       let n = Math.round(Math.random() * l);
       return colors[n];
     } else {
-      return 'rgb(' +
+      return (
+        'rgb(' +
         this.getRandom(255) +
         ',' +
         this.getRandom(255) +
         ',' +
         this.getRandom(255) +
-        ')';
+        ')'
+      );
     }
-  }
+  };
 
   explosionBox = (color) => {
-    const { circular, iterations } = this.props;
+    const {circular, iterations} = this.props;
     let ballSize = this.movingBall.interpolate({
       inputRange: [0, 1],
-      outputRange: [0, 12]
+      outputRange: [0, 12],
     });
     let ballRadius = this.movingBall.interpolate({
       inputRange: [0, 1],
-      outputRange: [0, 6]
+      outputRange: [0, 6],
     });
     let balls = [],
       randomTops = [],
       randomLefts = [],
-      x, y, loopCount;
+      x,
+      y,
+      loopCount;
     loopCount = circular ? 20 : 30;
     for (let i = 0; i <= loopCount; i++) {
       balls.push('');
@@ -162,14 +164,13 @@ export default class Fireworks extends Component {
         y = yPositions[i];
         randomTops[i] = this.movingBall.interpolate({
           inputRange: [0, 1],
-          outputRange: [100, y]
+          outputRange: [100, y],
         });
         randomLefts[i] = this.movingBall.interpolate({
           inputRange: [0, 1],
-          outputRange: [100, x]
+          outputRange: [100, x],
         });
-      }
-      else {
+      } else {
         randomTops[i] = this.movingBall.interpolate({
           inputRange: [0, 1],
           outputRange: [100, this.getRandom(200)],
@@ -199,7 +200,7 @@ export default class Fireworks extends Component {
                     top: randomTops[index],
                     left: randomLefts[index],
                     opacity: circular ? 1 : ballOpacity,
-                    backgroundColor: color || this.getRandomColors(),
+                    backgroundColor: this.getRandomColors(),
                   },
                 ]}
               />
@@ -211,8 +212,8 @@ export default class Fireworks extends Component {
     return null;
   };
   render() {
-    const { x, y } = this.state;
-    const { iterations } = this.props;
+    const {x, y} = this.state;
+    const {iterations} = this.props;
     if (!iterations || count < iterations) {
       return (
         <View style={styles.container}>
@@ -253,4 +254,3 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
-
